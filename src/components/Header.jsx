b2f21b724fc5196165f336d703010df54bb7f866
iredox <idreesadam200@gmail.com>
2024-09-17 12:44:34 +0100

    run build command

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../assets/baze_logo.png'
 
 const Header = () => {
   return (
-    <div>
-      <div>logo</div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/about">Services</Link>
+    <div className="flex items-center justify-between p-5">
+      <div className="w-[10%]">
+        <img src={logo} alt="" className="w-full" />
+      </div>
+      <div className="flex items-center gap-4 ">
+        <Link className="text-white hover:text-gray-300" to="/">Home</Link>
+        <Link className="text-white hover:text-gray-300" to="/about">About</Link>
+        <Link className="text-white hover:text-gray-300" to="/about">Services</Link>
       </div>
     </div>
   );
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Header from '../components/Header'
+import Search from '../components/Search'
+import book1 from '../assets/book1.jpeg'
 
 const Book = () => {
   return (
-    <div>Book</div>
+    <div>
+        <Header />
+        <div className='my-22'>
+            <div className='flex p-10'>
+                <div>
+                    <div className='w-[40%] '>
+                        <img src={book1} alt="" className='w-full'/>
+                        <button className='text-white bg-green-400 block my-5 py2 px-4'>DownLoad</button>
+                    </div>
+                </div>
+                <div className='capitalize bg-green-100 p-2'>
+                    <p>Book Detail</p>
+                    <p><span className='font-bold'>Book Name:</span>Essential Computer Science</p>
+                    <p><span className='font-bold'>Author:</span>Pual d.</p>
+                    <p><span className='font-bold'>pages</span>350p</p>
+                    <p><span className='font-bold'>full description</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus deleniti explicabo aspernatur praesentium incidunt soluta aperiam quod atque autem! Eligendi, enim delectus? Blanditiis officia dolor corporis nihil esse illum cum error a inventore sint? Neque dolorem tempora molestias ratione consequuntur?</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
